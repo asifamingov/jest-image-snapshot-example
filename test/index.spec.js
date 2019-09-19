49640fe25f834406e4ef5eea9e660725c28f91b6
AsifAmin <asif_udr@yahoo.com>
2019-09-19 15:07:12 +1000

    dsf

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,17 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+
+expect.extend({ toMatchImageSnapshot });
 
 it('Visual regression test', async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: './node_modules/chromium/lib/chromium/chrome-linux/chrome',
+  });
   const page = await browser.newPage();
-  await page.goto('http://localhost:3000');
-  const screenshot = await page.screenshot();
+  await page.setViewport({ width: 1200, height: 1800 });
+  await page.goto('http://localhost:8089/index-page.html');
+  const image = await page.screenshot({ fullPage: true });
   browser.close();
 
-  expect(screenshot).toMatchImageSnapshot();
+  expect(image).toMatchImageSnapshot();
 });
